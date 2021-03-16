f836d0f4a9ccfdd97afa2e2c829f1a3a83b8f69e
Yuma Miyata <kabosu910@gmail.com>
2021-03-16 20:25:32 +0900

    Add unary plus and minus [step:6]

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -48,8 +48,9 @@ Token *token;
 char *user_input;
 
 Node *expr();
-Node *primary();
 Node *mul();
+Node *unary();
+Node *primary();
 
 // エラーを報告するための関数
 void error_at(char *loc, char *fmt, ...) {
@@ -168,16 +169,24 @@ Node *expr() {
 }
 
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 
 	for (;;) {
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
+}
+
+Node *unary() {
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
 }
 
 Node *primary() {
--- 9cc/test.sh
+++ 9cc/test.sh
@@ -21,5 +21,8 @@ assert 41 " 12 + 34 - 5 "
 assert 47 "5+6*7"
 assert 15 "5*(9-6)"
 assert 4 "(3+5)/2"
+assert 10 "-10+20"
+assert 40 "10*-2*-2"
+assert 20 "-10*-2"
 
 echo OK
